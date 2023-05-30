acabfcd7fa6f9654d7223378de27c92e2830d09f
Yao Fu <fubupc@gmail.com>
2023-05-30 09:44:35 +0800

    Import: merge and fix os updates from official repo

--- os/kernel/src/main.rs
+++ os/kernel/src/main.rs
@@ -1,5 +1,6 @@
 #![feature(decl_macro)]
 #![feature(negative_impls)]
+#![feature(allocator_api)]
 #![no_std]
 #![no_main]
 
@@ -8,14 +9,28 @@ fn panic_handler(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+pub mod allocator;
 pub mod console;
+pub mod fs;
 pub mod mutex;
 pub mod shell;
 
 use core::arch::global_asm;
 global_asm!(include_str!("../ext/init.S"));
 
+#[cfg(not(test))]
+use allocator::Allocator;
+use fs::FileSystem;
+
+#[cfg(not(test))]
+pub static _ALLOCATOR: Allocator = Allocator::uninitialized();
+#[cfg(not(test))]
+#[global_allocator]
+pub static ALLOCATOR: &Allocator = &_ALLOCATOR;
+
+pub static FILE_SYSTEM: FileSystem = FileSystem::uninitialized();
 #[no_mangle]
 pub unsafe extern "C" fn kmain() -> ! {
+    // ALLOCATOR.initialize();
     shell::shell("> ")
 }
--- os/pi/src/lib.rs
+++ os/pi/src/lib.rs
@@ -5,6 +5,7 @@
 // #![feature(repr_align)]
 // #![feature(attr_literals)]
 #![feature(never_type)]
+#![feature(pointer_methods)]
 
 #![no_std]
 
@@ -16,3 +17,4 @@ pub mod timer;
 pub mod uart;
 pub mod gpio;
 pub mod common;
+pub mod atags;
