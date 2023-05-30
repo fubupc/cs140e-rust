2d633a3eb0d86a9fda600dbaaa50f932b8c8f0cf
Yao Fu <fubupc@gmail.com>
2023-05-30 09:44:35 +0800

    Import: merge os updates from official repo

--- os/kernel/build.rs
+++ os/kernel/build.rs
@@ -1,4 +1,10 @@
 pub fn main() {
+    if ::std::env::var("TARGET").unwrap() == "aarch64-unknown-none" {
+        println!("cargo:rustc-link-search=native=ext");
+        println!("cargo:rustc-link-lib=static=sd");
+        println!("cargo:rerun-if-changed=ext/libsd.a");
+    }
+
     println!("cargo:rerun-if-changed=ext/layout.ld");
     println!("cargo:rerun-if-changed=ext/init.S");
 }
--- os/kernel/src/main.rs
+++ os/kernel/src/main.rs
@@ -1,16 +1,32 @@
 #![feature(decl_macro)]
 #![feature(negative_impls)]
 #![feature(restricted_std)]
+#![feature(allocator_api)]
+#![feature(lang_items)]
 #![no_main]
 
+pub mod allocator;
 pub mod console;
+pub mod fs;
 pub mod mutex;
 pub mod shell;
 
 use core::arch::global_asm;
 global_asm!(include_str!("../ext/init.S"));
 
+#[cfg(not(test))]
+use allocator::Allocator;
+use fs::FileSystem;
+
+#[cfg(not(test))]
+pub static _ALLOCATOR: Allocator = Allocator::uninitialized();
+#[cfg(not(test))]
+#[global_allocator]
+pub static ALLOCATOR: &Allocator = &_ALLOCATOR;
+
+pub static FILE_SYSTEM: FileSystem = FileSystem::uninitialized();
 #[no_mangle]
 pub unsafe extern "C" fn kmain() -> ! {
+    // ALLOCATOR.initialize();
     shell::shell("> ")
 }
