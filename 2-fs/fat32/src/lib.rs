47d0bd7fd22beada568a2757018f5f0c8f722355
Yao Fu <fubupc@gmail.com>
2023-06-02 17:56:45 +0800

    Fix: 2-fs/fat32 use customized os/std lib

--- 2-fs/fat32/src/lib.rs
+++ 2-fs/fat32/src/lib.rs
@@ -1,5 +1,13 @@
-#![feature(decl_macro, conservative_impl_trait)]
-#![allow(safe_packed_borrows)]
+#![feature(decl_macro)]
+#![feature(prelude_import)]
+
+#![no_std]
+
+extern crate std;
+#[prelude_import]
+#[allow(unused)]
+use std::prelude::v1::*;
+
 
 #[cfg(not(target_endian="little"))]
 compile_error!("only little endian platforms supported");
