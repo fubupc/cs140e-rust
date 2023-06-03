f7bf98e54190b6e269a1cd2799a7aec19e196982
Yao Fu <fubupc@gmail.com>
2023-06-03 15:55:41 +0800

    align_up & align_down

--- os/kernel/src/allocator/util.rs
+++ os/kernel/src/allocator/util.rs
@@ -6,7 +6,20 @@
 ///
 /// Panics if `align` is not a power of 2.
 pub fn align_down(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    if align == 0 {
+        panic!("align_down: align 0 is not a power of 2");
+    }
+    let mut align = align;
+    let mut power: usize = 0;
+    while align != 1 {
+        if align & 0x1 == 1 {
+            panic!("align_down: align {} is not a power of 2", align);
+        }
+        align = align >> 1;
+        power += 1;
+    }
+
+    addr >> power << power
 }
 
 /// Align `addr` upwards to the nearest multiple of `align`.
@@ -17,5 +30,10 @@ pub fn align_down(addr: usize, align: usize) -> usize {
 ///
 /// Panics if `align` is not a power of 2.
 pub fn align_up(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    let down = align_down(addr, align);
+    if addr == down {
+        down
+    } else {
+        down + align
+    }
 }
