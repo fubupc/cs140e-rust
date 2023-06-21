a9497ee20e0aaf86a913e6b1309f87b63b43e1a0
Yao Fu <fubupc@gmail.com>
2023-06-21 09:52:51 +0800

    Assignment 2 > Phase 2: 32-bit Lipids > Implement `FatEntry`

--- 2-fs/fat32/src/vfat/fat.rs
+++ 2-fs/fat32/src/vfat/fat.rs
@@ -16,7 +16,7 @@ pub enum Status {
     Bad,
     /// The FAT entry corresponds to a valid data cluster. The corresponding
     /// cluster is the last in its chain.
-    Eoc(u32)
+    Eoc(u32),
 }
 
 #[repr(C, packed)]
@@ -25,15 +25,47 @@ pub struct FatEntry(pub u32);
 impl FatEntry {
     /// Returns the `Status` of the FAT entry `self`.
     pub fn status(&self) -> Status {
-        unimplemented!("FatEntry::status()")
+        match self.0 & 0x0FFFFFFF {
+            0x00000000 => Status::Free,
+            0x00000001 => Status::Reserved,
+            c @ 0x00000002..=0x0FFFFFEF => Status::Data(Cluster::from(c)),
+            0x0FFFFFF0..=0x0FFFFFF5 => Status::Reserved,
+            0x0FFFFFF6 => Status::Reserved,
+            0x0FFFFFF7 => Status::Bad,
+            e @ 0x0FFFFFF8..=0x0FFFFFFF => Status::Eoc(e),
+            _ => unreachable!(),
+        }
     }
 }
 
 impl fmt::Debug for FatEntry {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.debug_struct("FatEntry")
-            .field("value", &{self.0})
+            .field("value", &{ self.0 })
             .field("status", &self.status())
             .finish()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::vfat::{Cluster, FatEntry, Status};
+
+    #[test]
+    fn test_status() {
+        assert_eq!(FatEntry(0x00000000).status(), Status::Free);
+        assert_eq!(FatEntry(0x10000000).status(), Status::Free);
+
+        assert_eq!(
+            FatEntry(0x000001F6).status(),
+            Status::Data(Cluster::from(0x000001F6))
+        );
+        assert_eq!(
+            FatEntry(0x200001E2).status(),
+            Status::Data(Cluster::from(0x000001E2))
+        );
+
+        assert_eq!(FatEntry(0x0FFFFFF8).status(), Status::Eoc(0x0FFFFFF8));
+        assert_eq!(FatEntry(0x3FFFFFF9).status(), Status::Eoc(0x0FFFFFF9));
     }
 }
