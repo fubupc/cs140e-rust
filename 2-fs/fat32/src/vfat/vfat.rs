741cd486e46bcb38d72d7005ab9a22e005d37166
Yao Fu <fubupc@gmail.com>
2023-06-21 09:46:36 +0800

    Assignment 2 > Phase 2: 32-bit Lipids > Implement `VFat::from()`

--- 2-fs/fat32/src/vfat/vfat.rs
+++ 2-fs/fat32/src/vfat/vfat.rs
@@ -1,13 +1,18 @@
-use std::io;
-use std::path::Path;
-use std::mem::size_of;
+use core::convert::TryInto;
 use std::cmp::min;
+use std::io::{self, Read, Write};
+use std::mem::size_of;
+use std::path::Path;
 
-use util::SliceExt;
 use mbr::MasterBootRecord;
-use vfat::{Shared, Cluster, File, Dir, Entry, FatEntry, Error, Status};
+use traits::{BlockDevice, FileSystem};
+use util::SliceExt;
 use vfat::{BiosParameterBlock, CachedDevice, Partition};
-use traits::{FileSystem, BlockDevice};
+use vfat::{Cluster, Dir, Entry, Error, FatEntry, File, Shared, Status};
+
+use super::cluster;
+
+const FAT_ENTRY_SIZE: u64 = size_of::<FatEntry>() as u64;
 
 #[derive(Debug)]
 pub struct VFat {
@@ -22,9 +27,51 @@ pub struct VFat {
 
 impl VFat {
     pub fn from<T>(mut device: T) -> Result<Shared<VFat>, Error>
-        where T: BlockDevice + 'static
+    where
+        T: BlockDevice + 'static,
     {
-        unimplemented!("VFat::from()")
+        let mbr = MasterBootRecord::from(&mut device)?;
+
+        // Locate the first FAT32 partition
+        let pe = mbr
+            .partitions
+            .iter()
+            .find(|p| matches!(p.partition_type, 0xB | 0xC))
+            .ok_or(Error::NotFound)?;
+
+        let bpb = BiosParameterBlock::from(&mut device, pe.relative_sector as u64)?;
+
+        if pe.total_sectors as u64
+            != (bpb.bytes_per_sector as u64 * bpb.total_sectors_32 as u64 / device.sector_size())
+        {
+            panic!("partition size between MBR and BPB mismatch");
+        }
+
+        // Some entries might be empty in the last secotor of FAT
+        let max_clusters =
+            bpb.sectors_per_fat_32 as u64 * bpb.bytes_per_sector as u64 / FAT_ENTRY_SIZE;
+        assert!(
+            bpb.total_sectors_32 as u64
+                <= bpb.reserved_sectors as u64
+                    + bpb.number_of_fats as u64 * bpb.sectors_per_fat_32 as u64
+                    + bpb.sectors_per_cluster as u64 * max_clusters
+        );
+
+        let partition = Partition {
+            start: pe.relative_sector as u64, // physical starting sector of partition
+            sector_size: bpb.bytes_per_sector as u64,
+        };
+
+        Ok(Shared::new(VFat {
+            device: CachedDevice::new(device, partition),
+            bytes_per_sector: bpb.bytes_per_sector,
+            sectors_per_cluster: bpb.sectors_per_cluster,
+            sectors_per_fat: bpb.sectors_per_fat_32,
+            fat_start_sector: bpb.reserved_sectors as u64,
+            data_start_sector: bpb.reserved_sectors as u64
+                + bpb.number_of_fats as u64 * bpb.sectors_per_fat_32 as u64,
+            root_dir_cluster: Cluster::from(bpb.root_dir_cluster),
+        }))
     }
 
     // TODO: The following methods may be useful here:
@@ -67,13 +114,16 @@ impl<'a> FileSystem for &'a Shared<VFat> {
     }
 
     fn create_dir<P>(self, _path: P, _parents: bool) -> io::Result<Self::Dir>
-        where P: AsRef<Path>
+    where
+        P: AsRef<Path>,
     {
         unimplemented!("read only file system")
     }
 
     fn rename<P, Q>(self, _from: P, _to: Q) -> io::Result<()>
-        where P: AsRef<Path>, Q: AsRef<Path>
+    where
+        P: AsRef<Path>,
+        Q: AsRef<Path>,
     {
         unimplemented!("read only file system")
     }
