7ccaedb1e3c6168c92b3879b972c2861804b2629
Yao Fu <fubupc@gmail.com>
2023-06-21 10:04:31 +0800

    Assignment 2 > Phase 2: 32-bit Lipids > Implement `VFat::(fat_entry|read_cluster|read_chain)`

--- 2-fs/fat32/src/vfat/cluster.rs
+++ 2-fs/fat32/src/vfat/cluster.rs
@@ -10,3 +10,8 @@ impl From<u32> for Cluster {
 }
 
 // TODO: Implement any useful helper methods on `Cluster`.
+impl Cluster {
+    pub fn inner(&self) -> u32 {
+        self.0
+    }
+}
--- 2-fs/fat32/src/vfat/vfat.rs
+++ 2-fs/fat32/src/vfat/vfat.rs
@@ -74,30 +74,101 @@ impl VFat {
         }))
     }
 
-    // TODO: The following methods may be useful here:
     //
     //  * A method to read from an offset of a cluster into a buffer.
     //
-    //    fn read_cluster(
-    //        &mut self,
-    //        cluster: Cluster,
-    //        offset: usize,
-    //        buf: &mut [u8]
-    //    ) -> io::Result<usize>;
+    pub fn read_cluster(
+        &mut self,
+        cluster: Cluster,
+        offset: usize,
+        buf: &mut [u8],
+    ) -> io::Result<usize> {
+        match self.fat_entry(cluster)?.status() {
+            Status::Data(_) | Status::Eoc(_) => {
+                let start_sector = self.cluster_start_sector(cluster.inner());
+
+                let sector_offset = offset / (self.bytes_per_sector as usize);
+                let offset_in_sector = offset % (self.bytes_per_sector as usize); // in bytes
+
+                assert!(sector_offset < self.sectors_per_cluster as usize);
+
+                let mut total = 0;
+                let mut buf = buf;
+                for i in sector_offset as u64..self.sectors_per_cluster as u64 {
+                    let mut sector = self.device.get(start_sector + i)?;
+                    let n = if i == 0 {
+                        (&sector[offset_in_sector..]).read(buf)?
+                    } else {
+                        sector.read(buf)?
+                    };
+                    total += n;
+                    buf = &mut buf[n..];
+                    if buf.len() == 0 {
+                        return Ok(total);
+                    }
+                }
+                Ok(total)
+            }
+            _ => Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!("cluster {} is not data cluster", cluster.inner()),
+            )),
+        }
+    }
+
     //
     //  * A method to read all of the clusters chained from a starting cluster
     //    into a vector.
     //
-    //    fn read_chain(
-    //        &mut self,
-    //        start: Cluster,
-    //        buf: &mut Vec<u8>
-    //    ) -> io::Result<usize>;
+    pub fn read_chain(&mut self, start: Cluster, buf: &mut Vec<u8>) -> io::Result<usize> {
+        let init_len = buf.len();
+        let mut curr = start;
+        let mut total = 0;
+
+        loop {
+            if buf.capacity() - buf.len() < self.cluster_size() {
+                buf.reserve_exact(self.cluster_size() - (buf.capacity() - buf.len()));
+                assert!(buf.capacity() - buf.len() >= self.cluster_size());
+            }
+
+            let n = self.read_cluster(curr, 0, &mut buf[init_len + total..])?;
+            assert!(n == self.cluster_size());
+            total += n;
+
+            match self.fat_entry(curr)?.status() {
+                Status::Eoc(_) => return Ok(total),
+                Status::Data(next) => curr = next,
+                _ => return Err(io::ErrorKind::InvalidData.into()),
+            }
+        }
+    }
+
     //
     //  * A method to return a reference to a `FatEntry` for a cluster where the
     //    reference points directly into a cached sector.
     //
-    //    fn fat_entry(&mut self, cluster: Cluster) -> io::Result<&FatEntry>;
+    pub fn fat_entry(&mut self, cluster: Cluster) -> io::Result<&FatEntry> {
+        let cluster = cluster.inner();
+
+        let sector_offset = (cluster as u64 * FAT_ENTRY_SIZE) / (self.bytes_per_sector as u64);
+        let byte_offset = (cluster as u64 * FAT_ENTRY_SIZE) % (self.bytes_per_sector as u64);
+
+        let sector = self.device.get(self.fat_start_sector + sector_offset)?;
+
+        let entry = unsafe {
+            &*(&sector[byte_offset as usize] as *const u8 as *const u32 as *const FatEntry)
+        };
+
+        Ok(entry)
+    }
+
+    fn cluster_size(&self) -> usize {
+        self.sectors_per_cluster as usize * self.bytes_per_sector as usize
+    }
+
+    fn cluster_start_sector(&self, cluster: u32) -> u64 {
+        self.data_start_sector + self.sectors_per_cluster as u64 * (cluster as u64 - 2)
+    }
 }
 
 impl<'a> FileSystem for &'a Shared<VFat> {
